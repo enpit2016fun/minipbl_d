8fa4269da0131633a3d535950b5f7d18a670fcf2
obalab <obalab@katoutakefumi-no-MacBook-Pro.local>
2016-08-25 02:25:02 +0900

    入力画面の内容を送信画面に反映させた

--- workshop/www/send.js
+++ workshop/www/send.js
@@ -3,8 +3,9 @@ window.onload = function() {
     /**
    * メールに記載する情報を格納する変数
    */
-    var address, ccAddress, subject, body, hiddenData;
+    var address, ccAddress, subject, body, hiddenData, mail;
     var sendmail = document.getElementById('mail');
+
 
     sendmail.onclick = function() {
 
@@ -13,7 +14,8 @@ window.onload = function() {
         address = 'g2116031@fun.ac.jp';
         ccAddress = 'g2116031@fun.ac.jp';
         subject = '件名';
-        body = '本文' + '%0D%0A' + hiddenData; // 「'%0D%0A'」を入れて改行
+	mail = document.fm.tarea.value
+        body = '本文' + '%0D%0A' + hiddenData + mail; // 「'%0D%0A'」を入れて改行
 
         // 「'?cc='」部分でCC追加
         location.href = 'mailto:' + address + '?cc=' + ccAddress + '&subject=' + subject + '&body=' + body;
