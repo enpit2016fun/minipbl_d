23e14ecb1507812d9f53f3ad6f784e046cec5be8
Kazuki Iwata <g2116005@fun.ac.jp>
2016-08-25 17:04:36 +0900

    master

---
+++ workshop/www/send.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+    /**
+   * メールに記載する情報を格納する変数
+   */
+    var address, ccAddress, subject, body, hiddenData, mail;
+    var sendmail = document.getElementById('mail');
+
+    sendmail.onclick = function() {
+
+        // メールに記載したい情報をhiddenタグから取得
+        hiddenData = document.getElementById('hidden_data').value;
+        address = 'enpit2016_minipbl_teamd@googlegroups.com';
+        ccAddress = '';
+        subject = '件名';
+	mail = document.fm.tarea.value
+        body = '本文' + '%0D%0A' + mail; // 「'%0D%0A'」を入れて改行
+
+        // 「'?cc='」部分でCC追加
+        location.href = 'mailto:' + address + '?cc=' + ccAddress + '&subject=' + subject + '&body=' + body;
+    };
+
+};
